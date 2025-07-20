7e1d87d5f6e8141c82af537863fb8e3fd1517de2
Lee Inho <inhoking01@gmail.com>
2025-07-20 22:39:58 +0900

    feat. 커스텀 탭 뷰 구현

--- PaleTalk/Presentation/Views/ContentView.swift
+++ PaleTalk/Presentation/Views/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var authViewModel: AuthViewModel
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        switch authViewModel.loginStatus {
+        case .googleSignedIn, .appleSignedIn:
+            CustomTabView()
+        case .signedOut:
+            SignInView()
+        case .initializing:
+            EmptyView()
         }
-        .padding()
+    }
+}
+
+struct CustomTabView: View {
+    @EnvironmentObject var authViewModel: AuthViewModel
+    @State private var selection = 0
+
+    var body: some View {
+        TabView(selection: $selection) {
+            EmptyView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+                .tag(0)
+            
+            SettingView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Setting")
+                }
+                .tag(1)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
