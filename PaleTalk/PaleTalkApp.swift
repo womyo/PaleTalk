f63d4052353037f62e58841820aafa608d4c71e5
Lee Inho <inhoking01@gmail.com>
2025-07-20 22:22:46 +0900

    feat. init project

---
+++ PaleTalk/PaleTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaleTalkApp.swift
+//  PaleTalk
+//
+//  Created by 이인호 on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PaleTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaleTalkTests/PaleTalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaleTalkTests.swift
+//  PaleTalkTests
+//
+//  Created by 이인호 on 7/20/25.
+//
+
+import Testing
+@testable import PaleTalk
+
+struct PaleTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
