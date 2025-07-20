82788853289a906ddebefda767f12a54df8aff7f
Lee Inho <inhoking01@gmail.com>
2025-07-20 23:55:48 +0900

    feat. 밤하늘 홈 화면 구현

--- PaleTalk/Application/PaleTalkApp.swift
+++ PaleTalk/Application/PaleTalkApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
+}
 
 @main
 struct PaleTalkApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject var authViewModel = AuthViewModel()
+    @StateObject var drawingViewModel = DrawingViewModel(usecase: DrawingRepository())
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(authViewModel)
+                .environmentObject(drawingViewModel)
+                .task {
+                    authViewModel.restore()
+                }
         }
     }
 }
