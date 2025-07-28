6c72799c30fe942bdf61b24f3cf025acab1a1846
Lee Inho <inhoking01@gmail.com>
2025-07-28 23:01:42 +0900

    feat. 설정 화면에 유저 프로필 추가

--- PaleTalk/Presentation/Views/Setting/SettingView.swift
+++ PaleTalk/Presentation/Views/Setting/SettingView.swift
@@ -6,24 +6,43 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct SettingView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
     @State var showLogoutAlert = false
     
     var body: some View {
-        Button {
-            showLogoutAlert = true
-        } label: {
-            Text("로그아웃")
-        }
-        .alert("로그아웃 하시겠습니까?", isPresented: $showLogoutAlert) {
-            Button("로그아웃") {
-                Task {
-                    try await authViewModel.signOut()
-                }
+        VStack {
+            VStack {
+                KFImage(URL(string: authViewModel.currentUser?.profileImageUrl ?? ""))
+                    .loadDiskFileSynchronously(true)
+                    .placeholder {
+                        Image(systemName: "photo")
+                    }
+                    .cacheMemoryOnly()
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .background(Color.gray.opacity(0.3))
+                    .clipShape(Circle())
+                
+                Text(authViewModel.currentUser?.nickname ?? "")
             }
-            Button("취소", role: .cancel) {}
+            
+            Button {
+                showLogoutAlert = true
+            } label: {
+                Text("로그아웃")
+            }
+            .alert("로그아웃 하시겠습니까?", isPresented: $showLogoutAlert) {
+                Button("로그아웃") {
+                    Task {
+                        await authViewModel.signOut()
+                    }
+                }
+                Button("취소", role: .cancel) {}
+            }
         }
     }
 }
